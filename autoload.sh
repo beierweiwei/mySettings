65d3f2d141ac61deb1e8365a5dc0a80aef8280b8
root <root@A010286-NC.ESG.360ES.CN>
2019-12-12 20:50:22 +0800

    new config

--- autoload.sh
+++ autoload.sh
@@ -2,5 +2,7 @@
 cp ~/.tmux.conf ./linux
 cp ~/.zshrc ./linux
 cp ~/.vimrc ./linux
+cp /mnt/c/Users/wangjunjie/cmder.setting.xml  ~/mySettings/windows
 git add -A
 git commit -m 'new config'
+git push
